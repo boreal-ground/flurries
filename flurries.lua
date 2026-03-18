9c6673ed0a5923bf12c7c1a81455c69ba3f131c5
Rob Scott <61803466+rwhscott@users.noreply.github.com>
2026-03-18 11:01:39 +0000

    added clock multiplier/dividers

--- flurries.lua
+++ flurries.lua
@@ -1,11 +1,14 @@
 -- flurries for iii - boreal ground
-print("flurries v0.4")
+print("flurries v0.5")
 
 -- editable parameters
 local internal_clock_bpm = 120 -- set starting bpm for internal clock
 local run_clock = true -- clock override: stops listening to all clocks if false
 local loop_start = 1 -- first step of loop
 local loop_end = 16 -- last step of loop
+local clock_div = 1 -- multiply/divide clock
+local clock_led_loc = 3 -- default position (x1)
+local divs = {4, 2, 1, 0.5, 0.25}
 
 -- starting parameters
 local clock_led = 0
@@ -28,14 +31,28 @@ local MIDI_STOP = 252
 function redraw_grid()
     grid_led_all(0)
 
-    -- clock led
-    if run_clock then
-        -- led 1,1 blink on clock running
-        grid_led(1, 1, clock_led * 15)
-    else
-        -- led 1,1 lit when run_clock is false
-        grid_led(1, 1, 15)
+    -- row 1 leds
+
+    -- clock divider / multiplier leds
+    for i = 1, 5 do
+        grid_led(i, 1, 4)
     end
+
+    -- clock led
+    -- UPDATE: move the clock led to the currently selected multiplier led
+    if run_clock then
+        -- active clock led blinks on clock running
+        grid_led(clock_led_loc, 1, clock_led * 15)
+        -- led 16, 1 is dim
+        grid_led(16, 1, 4)
+    else
+        -- inactive clock led lit when run_clock is false
+        grid_led(clock_led_loc, 1, 15)
+        -- led 16, 1 is bright
+        grid_led(16, 1, 15)
+    end
+
+    -- row 2 leds 
 
     -- light leds within active loop
     local step_min = math.min(loop_start, loop_end)
@@ -93,7 +110,7 @@ end
 -- MAIN SCRIPT
 
 -- initialise internal clock
-local internal_clock = metro.init(tick, 30 / internal_clock_bpm)
+local internal_clock = metro.init(tick, (30 / internal_clock_bpm) * clock_div)
 
 -- run internal clock on script launch
 check_direction()
@@ -112,9 +129,21 @@ function event_grid(x, y, z)
     if y == 1 then
         -- menu row: row 1
 
-        if x == 1 and z == 1 then
+        -- clock multiplier / divider
+        if z == 1 then
+            if x >= 1 and x <= 5 then
+                clock_div = divs[x]
+                clock_led_loc = x -- move LED to pressed button
+            end
+            local new_time = (30 / internal_clock_bpm) * clock_div
+            if internal_clock.time ~= new_time then
+                internal_clock.time = new_time
+            end
+        end
 
-            -- if button 1,1 is pressed: toggles run_clock override
+        -- if button 16,1 is pressed: toggles run_clock override
+        if x == 16 and z == 1 then
+
             run_clock = not run_clock
             print("clock running: " .. tostring(run_clock))
 
@@ -196,8 +225,9 @@ function event_midi(d1, d2, d3)
     elseif d1 == MIDI_CLOCK then
         -- ignore midi clock when run_clock is false
         if run_clock then
+            local midi_step_div = math.max(1, math.floor(12 * clock_div))
             midi_sync = true
-            midi_ppqn = (midi_ppqn + 1) % 12
+            midi_ppqn = (midi_ppqn + 1) % midi_step_div
             if midi_ppqn == 0 then
                 tick()
             end
